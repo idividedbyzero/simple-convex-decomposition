69313dee60d5052d9b6bb7962520f46a05c0cadc
Oliver Heinzel <heio@cit.tum.de>
2024-04-03 15:39:12 +0200

    outsourced closed_form

--- make_inequality_constraints.py
+++ make_inequality_constraints.py
@@ -3,14 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import List
 from scipy.optimize import LinearConstraint, minimize, OptimizeResult
-import pygame
 from run import making_polygon
 import polygon
 import polygontransform
 
-import itertools
-
 def from_vertices_to_constraints(vertices: np.ndarray, show: bool=True)->list[tuple]: 
+    """Generate vectors a_i, b_i for inequality constraints <a_i,x><=b, simply form the vertices of a convex polygon
+
+    Args:
+        vertices (np.ndarray): Vertices of the convex polygon of shape Nx2
+        show (bool, optional): Wether to show a plot of the situation. Defaults to True.
+
+    Returns:
+        list[tuple]: format ([...], .)=(a_i, b_i)
+    """
     # Initialize lists to store coefficients
     coefficients = []
 
@@ -56,27 +62,6 @@ def from_vertices_to_constraints(vertices: np.ndarray, show: bool=True)->list[tu
         plt.show()
     return coefficients
 
-def nonempty_subsets(n:int)->List:
-    # Generate all subsets of {0, 1, ..., n}
-    all_subsets = itertools.chain.from_iterable(itertools.combinations(range(0, n), r) for r in range(n+1)) 
-    nonempty_subsets = filter(lambda x: len(x) > 0, all_subsets)
-    return [np.array(a) for a in list(nonempty_subsets)]
-
-def get_G(a_s: List[tuple])->np.ndarray:
-    #Obtain the G matrix in the paper after numbering (26)
-    return np.dot(a_s, a_s.T)
-
-def is_singular(matrix):
-    if matrix.ndim == 1:  # Handle the case when matrix is a 1D array
-        matrix = np.reshape(matrix, (1, 1))  # Reshape to a 1x1 matrix
-    return np.linalg.det(matrix) == 0
-
-def solve_linear_system(small_G, RHS):
-    if small_G.ndim == 1:  # Handle the case when small_G is a 1D array
-        small_G = np.reshape(small_G, (1, 1))  # Reshape to a 1x1 matrix
-        RHS = np.reshape(RHS, (1,))  # Reshape RHS to a 1D array
-    return np.linalg.solve(small_G, RHS)
-
 def project_convex(x: np.ndarray, coeffs: List[tuple])->OptimizeResult:
     """Solves the quadtratic Programming min_v |x-v| s.t. <a_i,v><=b_i
     Args:
@@ -91,29 +76,18 @@ def project_convex(x: np.ndarray, coeffs: List[tuple])->OptimizeResult:
     lc=LinearConstraint(a_s, ub=b_s)
     loss=lambda v: 1/2*np.linalg.norm(x-v, ord=2)**2
     return minimize(loss, x, constraints=lc)
-    #Using the paper: https://arxiv.org/pdf/1607.00102.pdf
-    #TODO if necessary for banach spaces...still not working e.g for [-10, 1]
-    # n=len(coeffs)
-    # Delta=nonempty_subsets(n)
-    # a_s = np.array([_c[0] for _c in coeffs])
-    # b_s = np.array([_c[1] for _c in coeffs])
-    # G=get_G(a_s)
-    # for m_, I_ in enumerate(Delta):
-    #     #P=len(I_)
-    #     small_G=G[np.ix_(I_, I_)]
-    #     if is_singular(small_G):  #np.linalg.det(matrix) == 0 should suffice
-    #         continue
-    #     RHS=np.einsum("i,ki->k", x, a_s[I_])-b_s[I_] #(P)
-    #     mu=solve_linear_system(small_G, RHS) # handle one dimensional case
-    #     if np.any(mu<=0):
-    #         continue
-    #     Ic=[i_ for i_ in range(n) if i_ not in I_] #I_ complement
-    #     diff=x-np.einsum("i,ij->j", mu, a_s[I_])
-    #     if np.all(np.einsum("i,qi->q", diff, a_s[Ic])<=0):
-    #         return diff
-    # raise Exception("There must be at least one index set, satisfying the equations")
 
 def project_polygon(x: np.ndarray, poly: polygon.Polygon, show:bool=True)->np.ndarray:
+    """Returns a projection of x onto some polygon.
+
+    Args:
+        x (np.ndarray): Vector to be projected
+        poly (polygon.Polygon): Polygon to project onto
+        show (bool, optional): Wether to show a plot of the situation. Defaults to True.
+
+    Returns:
+        np.ndarray: A vector on the polygon closest to the point x
+    """
     cpg = polygon.ConvexPolygonsGroup(polygon.SimplePolygon(poly))
     #TODO in order to make this work one requires, to check if the polygon were made in the correct order
     p = polygontransform.ConvexPolygonGroup(cpg).poly_array
@@ -142,9 +116,6 @@ def project_polygon(x: np.ndarray, poly: polygon.Polygon, show:bool=True)->np.nd
         plt.gca().set_aspect('equal', adjustable='box')
         plt.show()
     return projected
-    
-    
-    
 
 
 if __name__=="__main__":
@@ -152,6 +123,6 @@ if __name__=="__main__":
     #vertices = np.array([[0, 0], [0, 5], [10, 5], [5, 2], [10, 0], [0, 0]])
     x=np.array([0,0])    
     res = making_polygon()
-    print(project_polygon(x, res))
+    project_polygon(x, res)
 
     
